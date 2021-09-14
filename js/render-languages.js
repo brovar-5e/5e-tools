d9f6c28bd16bedd2292e938848b63ed5e196fae4
Tomasz Biernacki <Tomasz.Biernacki@fenergo.com>
2021-09-14 11:00:25 +0200

    My very own 5e-tools copy - initial commit

---
+++ js/render-languages.js
@@ -0,0 +1,4 @@
+class RenderLanguages{static $getRenderedLanguage(it){return $$`${Renderer.utils.getBorderTr()}
+		${Renderer.language.getRenderedString(it)}
+		${Renderer.utils.getPageTr(it)}
+		${Renderer.utils.getBorderTr()}`}}
